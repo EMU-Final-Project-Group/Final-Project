2227d8a10a51cc10d3aa4de0c3cfa65e2a7e42eb
Jmeads4 <jmeads4@emich.edu>
2022-03-30 10:33:15 -0400

    Added Ari's stuff

---
+++ MainTest/Assets/Scenes/UI/uiTestScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class uiTestScript : MonoBehaviour
+{
+    public GameObject winScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        winScreen.SetActive(false);
+    }
+
+ 
+}
